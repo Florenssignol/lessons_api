62d94124083603e757ae8439464504f55556e399
Florence Schwartz <schflo@gmail.com>
2019-03-01 15:23:54 +0100

    configuration of the database and migration of the models

--- lessons/models.py
+++ lessons/models.py
@@ -3,9 +3,11 @@ from django.db import models
 class Account(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200, null=False)
-    email = models.Charfield(max_length=200, null=False)
-    password = models.Charfield(max_length=200, null=False)
-    Address = models.Charfiels(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=False)
+    password = models.CharField(max_length=255, null=False)
+    address = models.CharField(max_length=255, null=True)
+    insert_date = models.DateField(auto_now=True)
+    update_date = models.DateTimeField(auto_now=True)
 
 class Subscription(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -13,21 +15,42 @@ class Subscription(models.Model):
         'Account', 
         on_delete=models.CASCADE,
     )
-    status = models.CharField(max_lenght=45, null=False)
+    status = models.CharField(max_length=45, null=False)
     subscription_date = models.DateTimeField(auto_now=True)
+    status_id = models.ForeignKey(
+        'Status', 
+        on_delete=models.CASCADE,
+    )
+    insert_date = models.DateField(auto_now=True)
+    update_date = models.DateTimeField(auto_now=True)
 
 class Status(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200, null=False)
+    insert_date = models.DateField(auto_now=True)
+    update_date = models.DateTimeField(auto_now=True)
 
 class Lesson(models.Model):
     id = models.IntegerField(primary_key=True)
     date = models.DateTimeField(auto_now=False)
-    description = models.Charfield(max_length=200, null=False)
+    description = models.CharField(max_length=200, null=False)
+    subscription_id = models.ForeignKey(
+        'Subscription', 
+        on_delete=models.CASCADE,
+    )
+    insert_date = models.DateField(auto_now=True)
+    update_date = models.DateTimeField(auto_now=True)
 
 class Students(models.Model):
     id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=200, null=False)
     last_name = models.CharField(max_length=200, null=False)
     birthdate = models.DateField(auto_now=False)
-    email = models.Charfield(max_length=200, null=False)
+    email = models.CharField(max_length=200, null=False)
+    account_id = models.ForeignKey(
+        'Account', 
+        on_delete=models.CASCADE,
+    )
+    lessons = models.ManyToManyField(Lesson)
+    insert_date = models.DateField(auto_now=True)
+    update_date = models.DateTimeField(auto_now=True)
