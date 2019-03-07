80c4c2eb1d9cbc53f278fb5f319eea1d0e540653
Florence Schwartz <schflo@gmail.com>
2019-03-07 09:33:44 +0100

    completed serializer and view

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,11 +1,42 @@
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
-from lessons.models import Lesson
+from lessons.models import Lesson, Account, Subscription, Status, Students
 
 class LessonSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     date = serializers.DateTimeField()
     description = serializers.CharField(max_length=200)
+    insert_date = serializers.DateField()
+    update_date = serializers.DateTimeField()
+
+class AccountSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    email = serializers.CharField()
+    password = serializers.CharField()
+    address = serializers.CharField()
+    insert_date = serializers.DateField()
+    update_date = serializers.DateTimeField()
+
+class SubscriptionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    status = serializers.CharField()
+    subscription_date = serializers.DateTimeField()
+    insert_date = serializers.DateField()
+    update_date = serializers.DateTimeField()
+
+class StatusSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    insert_date = serializers.DateField()
+    update_date = serializers.DateTimeField()
+
+class StudentsSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    birthdate = serializers.DateField()
+    email = serializers.CharField()
     insert_date = serializers.DateField()
     update_date = serializers.DateTimeField()
--- lessons/views.py
+++ lessons/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.contrib.auth.models import User
-from lessons.models import Lesson
+from lessons.models import Lesson, Account, Subscription, Status, Students
 
 from lessons.serializers import LessonSerializer
 
@@ -28,6 +28,42 @@ class LessonView(APIView):
     def get(self, request):
         queryset = Lesson.objects.all()
         serializer = LessonSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        pass
+
+class AccountView(APIView):
+    def get(self, request):
+        queryset = Account.objects.all()
+        serializer = Accounterializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        pass
+
+class SubscriptionView(APIView):
+    def get(self, request):
+        queryset = Subscription.objects.all()
+        serializer = SubscriptionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        pass
+
+class StatusView(APIView):
+    def get(self, request):
+        queryset = Status.objects.all()
+        serializer = StatusSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        pass
+
+class StudentView(APIView):
+    def get(self, request):
+        queryset = Student.objects.all()
+        serializer = StudentSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def post(self, request):
